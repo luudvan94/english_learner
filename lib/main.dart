2c76db3432ae1f6fc4a2b9553a0f5edb9b2b0c9e
Van Luu <valu4298@gmail.com>
2019-04-01 11:46:13 -0500

    Adding bouncing animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:developer';
 
 void main() => runApp(MyApp());
 
@@ -6,106 +7,145 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyHomePageState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
-  void _incrementCounter() {
+  Offset controlPoint;
+  Offset prevPoint;
+  bool dragFromRight;
+
+  //ANIMATION
+  AnimationController _controller;
+  Animation<Offset> _animation;
+  Animation _curve;
+
+  @override
+  void initState() {
+    super.initState();
+    controlPoint = Offset(0, 0);
+    dragFromRight = true;
+  }
+
+  void _handleDragStart(ForcePressDetails details) {
+    debugPrint("drag start");
+    double width = MediaQuery.of(context).size.width;
+    double dx = details.globalPosition.dx;
+    if (dx < width/2) {
+      setState(() {
+        prevPoint = details.globalPosition;
+        controlPoint = Offset(0, details.globalPosition.dy);
+        dragFromRight = false;
+      });
+    } else {
+      setState(() {
+        prevPoint = details.globalPosition;
+        controlPoint = Offset(width, details.globalPosition.dy);
+        dragFromRight = true;
+      });
+    }
+  }
+
+  void _handleDragUpdate(ForcePressDetails details) {
+    debugPrint("drag update");
+    double dxOffset = details.globalPosition.dx - prevPoint.dx;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      prevPoint = details.globalPosition;
+      controlPoint = Offset(controlPoint.dx + dxOffset, details.globalPosition.dy);
     });
+  }
+
+  void _handleDragEnd(ForcePressDetails details) {
+    debugPrint("drag end");
+    Offset endPoint;
+    Size size = MediaQuery.of(context).size;
+    if (dragFromRight) {
+      endPoint = Offset(size.width, size.height/2);
+    } else {
+      endPoint = Offset(0, size.height/2);
+    }
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 380), vsync: this);
+    _curve = CurvedAnimation(parent: _controller, curve: Curves.bounceOut);
+    _animation = Tween<Offset>(
+      begin: controlPoint,
+      end: endPoint,
+    ).animate(_curve);
+
+    _animation.addListener(() {
+      setState(() {
+        controlPoint = _animation.value;
+      });
+    });
+    _controller.forward();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    // TODO: implement build
+    return GestureDetector(
+      onForcePressStart: this._handleDragStart,
+      onForcePressUpdate: this._handleDragUpdate,
+      onForcePressEnd: this._handleDragEnd,
+      child: CustomPaint(
+        painter: CustomBackground(controlPoint: controlPoint, fromRight: dragFromRight),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class CustomBackground extends CustomPainter {
+
+  CustomBackground({Key key, this.controlPoint = const Offset(0, 0), this.fromRight = true});
+
+  Offset controlPoint;
+  bool fromRight;
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = new Paint();
+    var path = new Path();
+    Offset startPoint;
+    Offset endPoint;
+    if (fromRight) {
+      startPoint = Offset(size.width, 0);
+      endPoint = Offset(size.width, size.height);
+
+    } else {
+      startPoint = Offset(0, 0);
+      endPoint = Offset(0, size.height);
+    }
+
+    var firstControlPoint = Offset(controlPoint.dx, controlPoint.dy - 20);
+    var secondControlPoint = Offset(controlPoint.dx, controlPoint.dy - 20);
+    path.moveTo(startPoint.dx, startPoint.dy);
+    path.cubicTo(firstControlPoint.dx, firstControlPoint.dy, secondControlPoint.dx, secondControlPoint.dy, endPoint.dx, endPoint.dy);
+
+    canvas.drawPath(path, paint);
+    canvas.drawColor(Colors.blueAccent, BlendMode.color);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
   }
 }
